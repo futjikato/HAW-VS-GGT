e1e80f05a392c61ba6bbd575ab62d557c7c2d5ca
Moritz Spindelhirn <van_hauten@web.de>
2014-06-11 17:06:00 +0200

    post practi ggt

--- src/ggt.erl
+++ src/ggt.erl
@@ -27,88 +27,94 @@
 
 
 start(StarterNumber, GgtNumber, TTW, TTT, Nameserver, Coordinator) ->
-  init(io_lib:format("39~s_~s", [GgtNumber, StarterNumber]), TTW, TTT, Nameserver, Coordinator).
+  init(lists:concat(["39",GgtNumber,'_', StarterNumber]), TTW, TTT, Nameserver, Coordinator, "init").
 
 
-init(name, TTW, TTT, Nameserver, Coordinator) ->
-  Coordinator ! {check_in, name},
-  log(name, "~s~s checking in to coordinator ~s as service '~s':~s", [?I, self(), Coordinator, name, now()]),
-  Nameserver ! {self(),{rebind, name, node()}},
-  log(name, "~sregistered at ~s:~s", [?I, Nameserver, now()]),
-  global:register_name(name, self()),
-  log("~s~s globally registered as ~s:~s", [?I, self(), name, now()]),
+init(Name, TTW, TTT, Nameserver, Coordinator, Status) ->
+  register(list_to_atom(Name), self()),
+  Coordinator ! {check_in, list_to_atom(Name)},
+  log(Name, "~s~p checking in to coordinator ~p as service '~s':~s", [?I, self(), Coordinator, Name, werkzeug:timeMilliSecond()]),
+  Nameserver ! {self(),{rebind, list_to_atom(Name), node()}},
+  log(Name, "~sregistered at ~p:~s", [?I, Nameserver, werkzeug:timeMilliSecond()]),
+  global:register_name(list_to_atom(Name), self()),
+  log(Name, "~s~p globally registered as ~s:~s", [?I, self(), Name, werkzeug:timeMilliSecond()]),
 
   receive
     {set_neighbours,LeftN,RightN} ->
-      log(name, "~sreceived neighbours and is changing to pre_process:~s", [?I, now()]),
-      pre_process(name, 0, TTW, TTT, Nameserver, Coordinator, LeftN, RightN);
+      log(Name, "~sreceived neighbours and is changing to pre_process:~s", [?I, werkzeug:timeMilliSecond()]),
+      pre_process(Name, 0, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, "received neighbous is now waiting for PMi");
 
     {whats_on, From} ->
-      From ! {i_am, "init"},
-      log(name, "~ssend status init to ~s:~s", [?I, From, now()]),
-      init(name, TTW, TTT, Nameserver, Coordinator);
+      From ! {i_am, Status},
+      log(Name, "~ssend status init to ~p:~s", [?I, From, werkzeug:timeMilliSecond()]),
+      init(Name, TTW, TTT, Nameserver, Coordinator, Status);
 
     {kill} ->
-      die(name, Nameserver)
+      die(Name, Nameserver)
   end.
 
-pre_process(name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN) ->
-  log(name, "~swaiting for Mi to be set:~s", [?PP, now()]),
+pre_process(Name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, Status) ->
+  log(Name, "~swaiting for Mi to be set:~s", [?PP, werkzeug:timeMilliSecond()]),
   receive
     {set_pmi, MiNeu} ->
-      process_pmi(name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, MiNeu);
+      log(Name, "~sreceived Mi = ~p:~s", [?PP, MiNeu, werkzeug:timeMilliSecond()]),
+      process_pmi(Name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, MiNeu);
 
     {whats_on, From} ->
       From ! {i_am, "pre_process"},
-      log(name, "~ssend status init to ~s:~s", [?PP, From, now()]),
-      pre_process(name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN);
+      log(Name, "~ssend status init to ~p:~s", [?PP, From, werkzeug:timeMilliSecond()]),
+      pre_process(Name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, Status);
 
     {kill} ->
-      die(name, Nameserver)
+      die(Name, Nameserver)
   end.
 
-process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi) ->
+process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, Status) ->
   receive
     {set_pmi, MiNeu} ->
-      process_pmi(name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, MiNeu);
+      process_pmi(Name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, MiNeu);
 
     {send,Y} ->
-      log(name, "~sreceived a send y=~s:~s", [?P, Y, now()]),
+      log(Name, "~sreceived a send y=~p:~s", [?P, Y, werkzeug:timeMilliSecond()]),
       if
         Y < Mi ->
-          log(name, "~scalculating new Mi:~s", [?P, now()]),
-          timer:sleep(TTW),
-          MiNew = (Mi-1) rem (Y+1),
+          log(Name, "~scalculating new Mi:~s", [?P, werkzeug:timeMilliSecond()]),
+	  PIDWorker = spawn(fun() -> worker(TTW, Mi, Y, self()) end),
+	  MiNew = dispatcherLoop(PIDWorker, Mi, Name, Nameserver),
+          %timer:sleep(TTW),
+          %MiNew = (Mi-1) rem (Y+1),
           LeftN ! {send, MiNew},
           RightN ! {send, MiNew},
-          Coordinator ! {brief_mi,{name, MiNew, now()}},
-          log(name, "~snotified neighbours and coordinator about new Mi = ~s:~s", [?P, MiNew, now()]),
+          Coordinator ! {brief_mi,{Name, MiNew, werkzeug:timeMilliSecond()}},
+          log(Name, "~snotified neighbours and coordinator about new Mi = ~p:~s", [?P, MiNew, werkzeug:timeMilliSecond()]),
           NewTimer = werkzeug:reset_timer(Timer, TTT * 1000, {start_vote}),
           TSN = now(),
-          log(name, "~stimer was reset:~s", [?P, now()]),
-          process(name, TSN, TTW, NewTimer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi);
+          log(Name, "~stimer was reset:~s", [?P, werkzeug:timeMilliSecond()]),
+          process(Name, TSN, TTW, NewTimer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, lists:concat(["calculated new Mi = ", MiNew]));
 
         true ->
-          process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi)
+          process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, "refused to work with new Y because it is smaller than Mi")
       end;
 
     {vote,Initiator} ->
-      log(name, "~sreceived a vote:~s", [?P, now()]),
+      log(Name, "~sreceived a vote:~s", [?P, werkzeug:timeMilliSecond()]),
       if
-        Initiator =:= name ->
-          Coordinator !{brief_term, {name, Mi, now()}, self()},
-          log(name, "~sinform coordinator about terminate:~s", [?P, now()]);
+        Initiator =:= Name ->
+          Coordinator !{brief_term, {Name, Mi, werkzeug:timeMilliSecond()}, self()},
+          log(Name, "~sinform coordinator about terminate:~s", [?P, werkzeug:timeMilliSecond()]),
+          process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, "informed coordinator about termination");
 
         true ->
           TSD = timer:now_diff(now(), TS),
           if
             TSD >= TTT div 2 ->
-              log(name, "~sinform neightbour about vote:~s", [?P, now()]),
+              log(Name, "~sinform neightbour about vote:~s", [?P, werkzeug:timeMilliSecond()]),
               LeftN ! {vote, Initiator},
-              process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi);
+              process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, "voted for termination");
 
           true ->
-            process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi)
+	    log(Name, "~sAbstimmung verworfen (letztes Event ist ~p ms her):~s", [?P, TSD, werkzeug:timeMilliSecond()]),
+            process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, "rejected vote to terminate")
           end
       end;
 
@@ -116,37 +122,54 @@ process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi) -
       TSD = timer:now_diff(now(), TS),
       if
         TSD >= TTT ->
-          log(name, "~sstarting a vote:~s", [?P, now()]),
-          LeftN ! {vote, name},
-          process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi);
+          log(Name, "~sstarting a vote:~s", [?P, werkzeug:timeMilliSecond()]),
+          LeftN ! {vote, Name},
+          process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, "started vote");
 
       true ->
-        process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi)
+        process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, Status)
       end;
 
     {tell_mi, From} ->
       From ! {mi, Mi},
-      process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi);
+      process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, Status);
 
     {whats_on,From} ->
-      From ! {i_am, "Process"},
-      process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi);
+      From ! {i_am, Status},
+      process(Name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, Mi, Status);
 
     {kill} ->
-      die(name, Nameserver)
+      die(Name, Nameserver)
   end.
 
-process_pmi(name, TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, MiNeu) ->
-  log(name, "~sreveived set_pmi ~s also reset the timer:~s", [?PP, MiNeu, now()]),
-  {ok, Timer} = timer:send_after(TTT * 1000, {start_vote}),
-  TS = now(),
-  process(name, TS, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, MiNeu).
+worker(TTW, Mi, Y, PID) ->
+  timer:sleep(TTW),
+  MiNew = (Mi-1) rem (Y+1),
+  PID ! {result, MiNew}.
 
-die(name, Nameserver) ->
-  log(name, "(dying)::unregistering ~s from ~s:~s", [name, Nameserver, now()]),
-  Nameserver ! {self(),{unbind,name}},
-  log(name, "(dying)::globally unbinding '~s' with ~s:~s", [name, self(), now()]),
-  global:unregister_name(name).
+dispatcherLoop(PIDW, Mi, Name, NS) -> 
+  receive
+    {whats_on, From} ->
+      From ! {i_am, "sleeping/working"},
+      dispatcherLoop(PIDW, Mi, Name, NS);
+    {result, MiNew} ->
+      MiNew;
+    {kill} ->
+      exit(PIDW, kill),
+      die(Name, NS)
+  end.
+
+process_pmi(Name, _TS, TTW, TTT, Nameserver, Coordinator, LeftN, RightN, MiNeu) ->
+  log(Name, "~sreveived set_pmi ~p also reset the timer:~s", [?PP, MiNeu, werkzeug:timeMilliSecond()]),
+  {ok, Timer} = timer:send_after(TTT * 1000, {start_vote}),
+  TSNew = now(),
+  process(Name, TSNew, TTW, Timer, TTT, Nameserver, Coordinator, LeftN, RightN, MiNeu, "got a new Mi to work with").
+
+die(Name, Nameserver) ->
+  log(Name, "(dying)::unregistering ~s from ~p:~s", [Name, Nameserver, werkzeug:timeMilliSecond()]),
+  Nameserver ! {self(),{unbind,list_to_atom(Name)}},
+  log(Name, "(dying)::globally unbinding '~s' with ~p:~s", [Name, self(), werkzeug:timeMilliSecond()]),
+  global:unregister_name(list_to_atom(Name)).
 
 
 %%----------------------------------------------------------------------
@@ -159,7 +182,7 @@ die(name, Nameserver) ->
 log(Name, Msg) ->
   log(Name, Msg, []).
 log(Name, Msg, []) ->
-  Filename = io_lib:format("~s_ggt_~p@~s.log", [Name, node(), ?HOSTNAME]),
+  Filename = io_lib:format("~s_ggt_~p.log", [Name, node()]),
   logging(Filename, Msg);
 log(Name, Msg, Params) ->
-  log(Name, io_lib:format("ggt:'~s' ~s", [Name, io_lib:format(Msg, Params)])).
+  log(Name, io_lib:format("ggt:'~s' ~s~n", [Name, io_lib:format(Msg, Params)])).
